f32cf41e9205c269dcd6d0d5f86c5f6cc6287594
samsisle <samko9522@gmail.com>
2019-02-04 02:50:46 -0800

    Add temp dummy styles [2]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import styles from './styles/App.module.css';
+
 export default function App() {
   return (
-    <div>
+    <div className={styles.test}>
       bruh!
     </div>
   )
 }
-
