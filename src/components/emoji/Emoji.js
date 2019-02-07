7d12841783dbff6a15adbeb93636c1104e3fe80f
samsisle <samko9522@gmail.com>
2019-02-07 15:55:51 -0800

    Add a tag + data-clipboard-text [3]

--- src/components/emoji/Emoji.js
+++ src/components/emoji/Emoji.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 export default function Emoji({ label, emoji }) {
   return (
-    <span
-      role="img"
-      aria-label={label ? label : ''}
-      aria-hidden={label ? 'false' : 'true'}
-    >
-      {emoji}
-    </span>
+    <a data-clipboard-text={emoji}>
+      <span
+        role="img"
+        aria-label={label ? label : ''}
+        aria-hidden={label ? 'false' : 'true'}
+      >
+        {emoji}
+      </span>
+    </a>
   );
 }
