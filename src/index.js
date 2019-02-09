7462091f06f3e76ce6f4d25282f2cdac50c2e867
samsisle <samko9522@gmail.com>
2019-02-08 16:40:32 -0800

    Remove import typeface-poppins [5]

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 import * as serviceWorker from "./services/serviceWorker";
-import "typeface-poppins";
 import "typeface-montserrat";
 import "./styles/reset.css";
 
