33d7c7d58d860be04ba48480a19de8e9086b1a05
CatsAreMLG <joshuadgon@gmail.com>
2019-02-08 18:15:47 -0600

    Add search bar icon to input

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -2,10 +2,13 @@ import React from "react";
 
 import styles from "./searchBar.module.css";
 
+const images = require("../../assets/svgs/" + "1f50d" + ".svg");
+
 export default function SearchBar() {
   return (
     <div className={styles.container}>
       <div className={styles.inputBox}>
+        <img className={styles.searchIcon} src={images} alt="search icon" />
         <input className={styles.input} placeholder="Search emoji" />
       </div>
     </div>
