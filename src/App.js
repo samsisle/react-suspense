c6bd43aa60dd66edfa2a8486c05a6a944eff87e1
samsisle <samko9522@gmail.com>
2019-02-08 19:10:42 -0800

    Add lazy-loaded dynamic imports with React Suspense

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
+import { Route } from "react-router-dom";
 
 import Banner from "./components/banner/Banner";
 import SearchBar from "./components/searchBar/SearchBar";
-import SmileysPeople from "./components/smileys_people/SmileysPeople";
-import Activity from "./components/activity/Activity";
-import AnimalsNature from "./components/animals_nature/AnimalsNature";
-import FoodDrink from "./components/food_drink/FoodDrink";
-import Objects from "./components/objects/Objects";
-import Symbols from "./components/symbols/Symbols";
-import TravelPlaces from "./components/travel_places/TravelPlaces";
-
 import styles from "./styles/App.module.css";
+
+/* prettier-ignore */
+const SmileysPeople = lazy(() => import("./components/smileys_people/SmileysPeople"));
+/* prettier-ignore */
+const Activity = lazy(() => import("./components/activity/Activity"));
+/* prettier-ignore */
+const AnimalsNature = lazy(() => import("./components/animals_nature/AnimalsNature"));
+/* prettier-ignore */
+const FoodDrink = lazy(() => import("./components/food_drink/FoodDrink"));
+/* prettier-ignore */
+const Objects = lazy(() => import("./components/objects/Objects"))
+/* prettier-ignore */
+const Symbols = lazy(() => import("./components/symbols/Symbols"))
+/* prettier-ignore */
+const TravelPlaces = lazy(() => import("./components/travel_places/TravelPlaces"))
 
 export default function App() {
   return (
     <div className={styles.test}>
       <Banner />
       <SearchBar />
-      <SmileysPeople />
-      <Activity />
-      <AnimalsNature />
-      <FoodDrink />
-      <Objects />
-      <Symbols />
-      <TravelPlaces />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Route exact path="/" component={SmileysPeople} />
+        <Route path="/activity" component={Activity} />
+        <Route path="/animals_nature" component={AnimalsNature} />
+        <Route path="/food_drink" component={FoodDrink} />
+        <Route path="/objects" component={Objects} />
+        <Route path="/symbols" component={Symbols} />
+        <Route path="/travel_places" component={TravelPlaces} />
+      </Suspense>
     </div>
   );
 }
