b7c51be810b4fb0a8d8c90af0e166e32cc5dca36
CatsAreMLG <joshuadgon@gmail.com>
2019-02-08 20:19:54 -0600

    Change input placeholder text

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -2,14 +2,23 @@ import React from "react";
 
 import styles from "./searchBar.module.css";
 
-const images = require("../../assets/svgs/" + "1f50d" + ".svg");
+const images = require("../../assets/svgs/" +
+  "1f50d" +
+  ".svg");
 
 export default function SearchBar() {
   return (
     <div className={styles.container}>
       <div className={styles.inputBox}>
-        <img className={styles.searchIcon} src={images} alt="search icon" />
-        <input className={styles.input} placeholder="Search emoji" />
+        <img
+          className={styles.searchIcon}
+          src={images}
+          alt="search icon"
+        />
+        <input
+          className={styles.input}
+          placeholder="Search for emojis"
+        />
       </div>
     </div>
   );
