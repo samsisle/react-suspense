454cb5cc1d6c01977a051d9b575358bad781dc32
CatsAreMLG <joshuadgon@gmail.com>
2019-02-08 20:20:50 -0600

    Remove comment from banner component

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -2,17 +2,20 @@ import React from "react";
 
 import styles from "./banner.module.css";
 
-const images = require("../../assets/svgs/" + "1f351" + ".svg");
+const images = require("../../assets/svgs/" +
+  "1f351" +
+  ".svg");
 
 const Banner = _ => {
   return (
     <div className={styles.banner}>
       <h1 className={styles.title}>
         em
-        {/* <span className={styles.titleEmoji} role="img" aria-label="peach">
-          🍑
-        </span> */}
-        <img className={styles.titleEmoji} src={images} alt="peach" />
+        <img
+          className={styles.titleEmoji}
+          src={images}
+          alt="peach"
+        />
         ji
       </h1>
       <p />
