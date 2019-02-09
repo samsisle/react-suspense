ec77f8658929316c4b5f9c23f758ee86190cea08
samsisle <samko9522@gmail.com>
2019-02-08 19:11:11 -0800

    Wrap BrowserRouter as Router to App component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 import * as serviceWorker from "./services/serviceWorker";
 import "typeface-montserrat";
 import "./styles/reset.css";
 
-ReactDOM.render(<App />, document.getElementById("suspense-app"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("suspense-app")
+);
 serviceWorker.register();
