e169426ae7fe4a61558d17b1fc666c2c7a3f08e9
samsisle <samko9522@gmail.com>
2019-02-11 23:37:29 -0800

    Format SearchBar component with new prettier configs

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -2,23 +2,14 @@ import React from "react";
 
 import styles from "./searchBar.module.css";
 
-const images = require("../../assets/svgs/" +
-  "1f50d" +
-  ".svg");
+const images = require("../../assets/svgs/" + "1f50d" + ".svg");
 
 export default function SearchBar() {
   return (
     <div className={styles.container}>
       <div className={styles.inputBox}>
-        <img
-          className={styles.searchIcon}
-          src={images}
-          alt="search icon"
-        />
-        <input
-          className={styles.input}
-          placeholder="Search for emojis"
-        />
+        <img className={styles.searchIcon} src={images} alt="search icon" />
+        <input className={styles.input} placeholder="Search for emojis" />
       </div>
     </div>
   );
