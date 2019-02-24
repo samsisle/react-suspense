ff40ea0b717aeeb7bb09ddf48acb18ced480e271
samsisle <samko9522@gmail.com>
2019-02-24 00:45:01 -0800

    Clean up Banner.js

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -8,13 +8,13 @@ import styles from "./banner.module.css";
 export default function Banner() {
   const [array] = useState(titleEmojis);
   const [index, setIndex] = useState(uniqueRNG(array.length - 1, 0, null));
+
   const image = require("../../assets/svgs/" + array[index].unicode + ".svg");
-  const handleIndex = _ => {
-    setIndex(uniqueRNG(array.length - 1, 0, index));
-  };
-  useInterval(_ => {
-    handleIndex(index);
-  }, 5000);
+
+  const handleIndex = _ => setIndex(uniqueRNG(array.length - 1, 0, index));
+
+  useInterval(_ => handleIndex(index), 5000);
+
   return (
     <div className={styles.banner}>
       <h1 className={styles.title}>
