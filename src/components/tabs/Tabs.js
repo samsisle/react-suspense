9dfc4ebe28183ea33e4c68c69985c5997063f0fc
samsisle <samko9522@gmail.com>
2019-02-26 23:54:44 -0800

    Move images & tabs array to __data__ directory

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -1,27 +1,9 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import { images } from "../../__data__/tabImages";
+import { tabs } from "../../__data__/tabs";
 import styles from "./tabs.module.css";
-
-const images = [
-  require("../../assets/svgs/1f600.svg"),
-  require("../../assets/svgs/1f43b.svg"),
-  require("../../assets/svgs/1f354.svg"),
-  require("../../assets/svgs/26bd.svg"),
-  require("../../assets/svgs/1f698.svg"),
-  require("../../assets/svgs/1f4a1.svg"),
-  require("../../assets/svgs/1f523.svg")
-];
-
-const tabs = [
-  { route: "/smileys_people", alt: "grinning face" },
-  { route: "/animals_nature", alt: "bear face" },
-  { route: "/food_drink", alt: "hamburger" },
-  { route: "/activity", alt: "soccer ball" },
-  { route: "/travel_places", alt: "oncoming automobile" },
-  { route: "/objects", alt: "electric light bulb" },
-  { route: "/symbols", alt: "input symbol for symbols" }
-];
 
 export default function Tabs() {
   return (
