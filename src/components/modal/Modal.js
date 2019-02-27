d9c744351f53340353d0b0d6aa199cfca329c9d8
CatsAreMLG <joshuadgon@gmail.com>
2019-02-26 18:52:27 -0600

    Change modal styling

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -5,8 +5,8 @@ import styles from "./modal.module.css";
 export default function Modal({ emoji, label }) {
   return (
     <div className={styles.container}>
-      <span>copied: </span>
       <img className={styles.emojiImage} src={emoji} alt={label} />
+      <span>Copied to your clipboard!</span>
     </div>
   );
 }
