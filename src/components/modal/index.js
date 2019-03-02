8d69fbbd5d5a4f4a387bf3a1493fcbb930da2370
CatsAreMLG <joshuadgon@gmail.com>
2019-03-01 18:28:41 -0600

    Remove ModalStack

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,4 +1,3 @@
 export { default as Portal } from "./Portal";
 export { default as Context } from "./Context";
-export { default as ModalStack } from "./ModalStack";
 export { default as Modal } from "./Modal";
