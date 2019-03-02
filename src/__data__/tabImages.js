79b05a0f9f035ee42e8cc629fc00e991b6ba2d23
CatsAreMLG <joshuadgon@gmail.com>
2019-03-01 18:24:12 -0600

    Switch tab images around

--- src/__data__/tabImages.js
+++ src/__data__/tabImages.js
@@ -1,6 +1,6 @@
 export const images = [
-  require("../assets/svgs/1f43b.svg"),
   require("../assets/svgs/1f600.svg"),
+  require("../assets/svgs/1f43b.svg"),
   require("../assets/svgs/1f354.svg"),
   require("../assets/svgs/26bd.svg"),
   require("../assets/svgs/1f698.svg"),
