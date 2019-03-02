92f4901b63a5b39641ec10323e011827e7e03e50
samsisle <samko9522@gmail.com>
2019-03-01 16:37:36 -0800

    Update App.js from class ➡ hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Suspense } from "react";
+import React, { useState, Suspense } from "react";
 import { Route, Redirect } from "react-router-dom";
 
 import Banner from "./components/banner/Banner";
@@ -18,44 +18,39 @@ import {
 import Footer from "./components/footer/Footer";
 import styles from "./styles/App.module.css";
 
-export default class App extends Component {
-  state = { test: null };
+export default function App() {
+  const [test, setTest] = useState(null);
 
-  copyModal = (emoji, label) => {
-    this.setState({ test: { emoji, label } });
-  };
+  const copyModal = (emoji, label) => setTest({ emoji, label });
+  const redirect = () => <Redirect to="/smileys_people" />;
 
-  redirect = () => <Redirect to="/smileys_people" />;
-
-  render() {
-    return (
-      <div className={styles.test}>
-        <Banner />
-        <Portal>
-          {this.state.test && (
-            <Expire delay={2000}>
-              <Modal emoji={this.state.test} />
-            </Expire>
-          )}
-        </Portal>
-        <SearchBar />
-        <Tabs />
-        <Suspense
-          fallback={<div className={styles.loading}>Loading Emojis...</div>}
-        >
-          <Context.Provider value={this.copyModal}>
-            <Route exact path="/" render={this.redirect} />
-            <Route path="/smileys_people" component={SmileysPeople} />
-            <Route path="/activity" component={Activity} />
-            <Route path="/animals_nature" component={AnimalsNature} />
-            <Route path="/food_drink" component={FoodDrink} />
-            <Route path="/objects" component={Objects} />
-            <Route path="/symbols" component={Symbols} />
-            <Route path="/travel_places" component={TravelPlaces} />
-          </Context.Provider>
-        </Suspense>
-        <Footer />
-      </div>
-    );
-  }
+  return (
+    <div className={styles.test}>
+      <Banner />
+      <Portal>
+        {test && (
+          <Expire delay={2000}>
+            <Modal emoji={test} />
+          </Expire>
+        )}
+      </Portal>
+      <SearchBar />
+      <Tabs />
+      <Suspense
+        fallback={<div className={styles.loading}>Loading Emojis...</div>}
+      >
+        <Context.Provider value={copyModal}>
+          <Route exact path="/" render={redirect} />
+          <Route path="/smileys_people" component={SmileysPeople} />
+          <Route path="/activity" component={Activity} />
+          <Route path="/animals_nature" component={AnimalsNature} />
+          <Route path="/food_drink" component={FoodDrink} />
+          <Route path="/objects" component={Objects} />
+          <Route path="/symbols" component={Symbols} />
+          <Route path="/travel_places" component={TravelPlaces} />
+        </Context.Provider>
+      </Suspense>
+      <Footer />
+    </div>
+  );
 }
