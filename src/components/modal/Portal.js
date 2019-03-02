abe923be10b89c3b70f48f3fc17c326230436f03
CatsAreMLG <joshuadgon@gmail.com>
2019-03-01 18:50:46 -0600

    Fix modal mobile styling

--- src/components/modal/Portal.js
+++ src/components/modal/Portal.js
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom";
+import "./portal.css";
 
 export default function Portal({ children }) {
   return ReactDOM.createPortal(children, document.getElementById("portal"));
