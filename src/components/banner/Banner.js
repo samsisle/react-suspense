da824bf09cb11f40d5006d05466d2823ee90aa49
samsisle <samko9522@gmail.com>
2019-03-08 11:37:05 -0800

    🚮 Remove outer <div> on share button

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -10,7 +10,6 @@ export default function Banner() {
   const [index, setIndex] = useState(uniqueRNG(array.length - 1, 0, null));
 
   const image = require("../../assets/svgs/" + array[index].unicode + ".svg");
-
   const handleIndex = _ => setIndex(uniqueRNG(array.length - 1, 0, index));
 
   useInterval(_ => handleIndex(index), 5000);
@@ -27,15 +26,14 @@ export default function Banner() {
         />
         ji
       </h1>
-      <div className={styles.share}>
-        <a
-          href="https://twitter.com/intent/tweet?text=feeling%20cozy%20and%20Emoji%20https%3A%2F%2Ftwemoji.netlify.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Share on Twitter
-        </a>
-      </div>
+      <a
+        className={styles.share}
+        href="https://twitter.com/intent/tweet?text=feeling%20cozy%20and%20Emoji%20https%3A%2F%2Ftwemoji.netlify.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Share on Twitter
+      </a>
     </div>
   );
 }
