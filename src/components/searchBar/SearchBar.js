7e36a38a96ae460516660d4fb9e63a2934a3d2a5
CatsAreMLG <joshuadgon@gmail.com>
2019-03-07 22:50:04 -0600

    Add fuse search functionality

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Fuse from "fuse.js";
 
+import { allEmojis as emojis } from "../../__data__/allEmojis";
 import styles from "./searchBar.module.css";
 
 const images = require("../../assets/svgs/1f50d.svg");
+const imageSrc = unicode => {
+  return require("../../assets/svgs/" + unicode + ".svg");
+};
 
 export default function SearchBar() {
+  const [search, setSearch] = useState("");
+
+  const options = {
+    keys: ["label"]
+  };
+  const fuse = new Fuse(emojis, options);
+  const handleSearch = e => {
+    setSearch(e.target.value);
+    console.log(fuse.search(search));
+  };
+
+  fuse.search("engsh");
   return (
     <div className={styles.container}>
       <div className={styles.inputBox}>
         <img className={styles.searchIcon} src={images} alt="search icon" />
-        <input className={styles.input} placeholder="Search for emojis" />
+        <input
+          className={styles.input}
+          placeholder="Search for emojis"
+          value={search}
+          onChange={handleSearch}
+        />
       </div>
     </div>
   );
