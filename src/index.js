9a9f935566e4d099d8864eab408076be57b02892
samsisle <samko9522@gmail.com>
2019-12-12 22:58:50 -0800

    ✨ Unlock concurrent mode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
@@ -7,7 +7,11 @@ import * as serviceWorker from './lib/serviceWorker';
 
 import './css/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
