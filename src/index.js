4025542cc75ec8cadd93182146d25b5029a2494d
samsisle <samko9522@gmail.com>
2019-12-12 22:12:56 -0800

    Move files to dirs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import * as serviceWorker from './lib/serviceWorker';
+
+import './css/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
